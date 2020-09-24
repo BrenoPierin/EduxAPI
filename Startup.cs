172959ecf9bbd2e3ec86af689be33d9fd6bf4dd4
guivolpe <guivolpe@gmail.com>
2020-09-24 10:42:10 -0300

    inclusão do swagger

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace EduxAPI
 {
@@ -25,6 +26,32 @@ namespace EduxAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Adicição do serviço Swagger - Guilherme
+            services.AddSwaggerGen();
+
+            // Adicionamos as informações gerais da API - Guilherme
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Edux API",
+                    Description = "Web Page simples para aprendizado em ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Grupo 6",
+                        Email = string.Empty,
+                        Url = new Uri("https://twitter.com/spboyer"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Open Licence",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +65,14 @@ namespace EduxAPI
             app.UseRouting();
 
             app.UseAuthorization();
+
+            // Habilitação do middleware para servir o Swagger gerado como um endpoint JSON. - Guilherme
+            app.UseSwagger();
+            //Configuramos o Endpoit e descrição. - Guilherme
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "API V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
