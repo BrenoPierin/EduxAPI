7510d436a42f50b631adfde3ed72627d118c44e9
guivolpe <guivolpe@gmail.com>
2020-09-27 15:06:00 -0300

    interfaces e repositorios

---
+++ Repositories/CategoriaRepository.cs
@@ -0,0 +1,123 @@
+﻿using EduxAPI.Contexts;
+using EduxAPI.Domains;
+using EduxAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduxAPI.Repositories
+{
+    public class CategoriaRepository : ICategoria
+    {
+        private readonly EduxContext _ctx;
+
+        public CategoriaRepository()
+        {
+            _ctx = new EduxContext();
+        }
+
+        /// <summary>
+        /// Metodo para alterar
+        /// </summary>
+        /// <param name="id">id do produto</param>
+        /// <param name="categoria">categoria</param>
+        /// <returns></returns>
+        public void Alterar(Guid id, Categoria categoria)
+        {
+            try
+            {
+                //Busca pelo seu Id
+                Categoria categoriaTemp = BuscarPorID(id);
+
+
+                //Altera as propriedades 
+                categoriaTemp.Tipo = categoria.Tipo;
+
+                //Altera no contexto
+                _ctx.Categoria.Update(categoriaTemp);
+                //Salva
+                _ctx.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                //TODO: Cadastrar Tabela LogErro  mensagem de erro com Tag Geral
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Metodo para buscar uma categoria pelo Id
+        /// </summary>
+        /// <param tipo="id">id da instituição</param>
+        /// <returns>tipo do id atribuido</returns>
+        public Categoria BuscarPorID(Guid id)
+        {
+            try
+            {
+
+                return _ctx.Categoria.Find(id);
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Metodo para adicionar novas categorias
+        /// </summary>
+        /// <param tipo="categoria">nova categoria</param>
+        public void Cadastrar(Categoria categoria)
+        {
+            try
+            {
+                _ctx.Categoria.Add(categoria);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// metodo para excluir
+        /// </summary>
+        /// <param tipo="id">id da instituição</param>
+        public void Excluir(Guid id)
+        {
+            try
+            {
+                Categoria categoria = BuscarPorID(id);
+
+                if (categoria == null)
+                    throw new Exception("Categoria não encontrada");
+
+                _ctx.Categoria.Remove(categoria);
+                _ctx.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// metodo para listar todos
+        /// </summary>
+        /// <returns>Lista de categorias</returns>
+        public List<Categoria> ListarTodos()
+        {
+            try
+            {
+                return _ctx.Categoria.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+
+}
---
+++ Repositories/CurtidaRepository.cs
@@ -0,0 +1,131 @@
+﻿using EduxAPI.Contexts;
+using EduxAPI.Domains;
+using EduxAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduxAPI.Repositories
+{
+    public class CurtidaRepository : ICurtida
+    {
+        private readonly EduxContext _ctx;
+        private object curtida;
+
+        public CurtidaRepository()
+        {
+            _ctx = new EduxContext();
+        }
+
+        /// <summary>
+        /// Metodo para alterar
+        /// </summary>
+        /// <param name="id">id do curso</param>
+ 
+        public void Alterar(Guid id, Curso curso)
+        {
+            try
+            {
+                //Busca pelo seu Id
+                Curtida curtidaTemp = BuscarPorID(id);
+
+
+                //Altera as propriedades 
+                curtidaTemp.IdUsuario = curtida.IdUsuario;
+                curtidaTemp.IdUsuarioNavigation = curtida.IdUsuarioNavigation;
+
+                curtidaTemp.IdDica = curtida.IdDica;
+                curtidaTemp.IdDicaNavigation = curtida.IdDicaNavigation;
+
+                //Altera no contexto
+                _ctx.Curtida.Update(curtidaTemp);
+
+                //Salva
+                _ctx.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                //TODO: Cadastrar Tabela LogErro  mensagem de erro com Tag Geral
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void Alterar(Guid id, Curtida curtida)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Metodo para buscar pelo id
+        /// </summary>
+        /// <param name="id">id do curso</param>
+        /// <returns>curso com determinado id</returns>
+        public Curtida BuscarPorID(Guid id)
+        {
+            try
+            {
+
+                return _ctx.Curtida.Find(id);
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// metodo para cadastrar
+        /// </summary>
+        /// <param name="curso">nome do curso</param>
+        public void Cadastrar(Curtida curtida)
+        {
+            try
+            {
+                _ctx.Curtida.Add(curtida);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// metodo para excluir
+        /// </summary>
+        /// <param name="id">id do curso</param>
+        public void Excluir(Guid id)
+        {
+            try
+            {
+                Curtida curtida = BuscarPorID(id);
+
+                if (curtida == null)
+                    throw new Exception("Sem Curtidas");
+
+                _ctx.Curtida.Remove(curtida);
+                _ctx.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// metodo para listar todos
+        /// </summary>
+        /// <returns>lista de curso</returns>
+        public List<Curtida> ListarTodos()
+        {
+            try
+            {
+                return _ctx.Curtida.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
